57d303489c4b433c6c459e19fa58a3df80763b52
aMOPel <momoPFL@gmail.com>
2023-02-05 18:15:27 +0100

    changed transpileMarkdown to use nosey.nim library to watch files. It also supports removing content files now.

--- README.md
+++ README.md
@@ -10,23 +10,35 @@ nimble install -l
 ```sh
 cd nim
 
-# (re)compile transpileMarkdown.nim
-nim c --outDir:bin --verbosity:0 --listfullpaths ./transpileMarkdown.nim
+# recompile and start markdown watcher
+# needs separate shell
+nim c --outDir:bin --verbosity:0 --listfullpaths ./transpileMarkdown.nim && ./bin/transpileMarkdown
 
 # start http server 
 # needs separate shell
 ./nimbledeps/bin/karun -r -w ./src/website.nim        
 
-# start markdown watcher
-# needs separate shell
-nim c --outDir:bin --verbosity:0 --listfullpaths ./transpileMarkdown.nim && ./bin/transpileMarkdown
-
 # start tailwind watcher
 # needs separate shell
 npx tailwindcss -i ./assets/prestyles.css -o ./assets/styles.css --watch
 
-# recompilation should work automatically because both build tools use the watch flag (-w/--watch)
+# recompilation should work automatically because all build tools use the watch flag (-w/--watch)
 # but if it doesn't work:
 # recompile website manually
 ./bin/transpileMarkdown && nim js --out:./app.js ./src/website.nim                  
+```
+
+Or convinient shell alias using `zsh` shell and `kitty` terminal:
+
+```sh
+website () {
+  p=~/Documents/website
+  pn=$p/nim
+  # opens windows for the 3 watchers
+  kitty @ launch --type=window --cwd=$pn --keep-focus zsh -c "nim c --outDir:bin --verbosity:0 --listfullpaths ./transpileMarkdown.nim && ./bin/transpileMarkdown"
+  kitty @ launch --type=window --cwd=$pn --keep-focus zsh -c "./nimbledeps/bin/karun -r -w ./src/website.nim"
+  kitty @ launch --type=window --cwd=$pn --keep-focus zsh -c "npx tailwindcss -i ./assets/prestyles.css -o ./assets/styles.css --watch"
+  # launches neovim session in a new tab afterwards
+  kitty @ launch --type=tab --cwd=$p zsh -c "nvim -S Session.vim"
+}
 ```
--- nim/transpileMarkdown.nim
+++ nim/transpileMarkdown.nim
@@ -1,19 +1,13 @@
-import std/[strformat, osproc, os, strutils, times, sha1, tables]
-import jsony
+import std/[strformat, osproc, os, strutils, times]
+import nosey
 
-const jsonFile = "../hashes.json"
-
-var contentFiles: Table[string, string]
-try:
-  contentFiles = readFile(jsonFile).fromJson(contentFiles.type)
-except IOError as e:
-  echo "ERROR: " & e.msg
-
-proc md2karax(dir: string, file: string) =
+proc mdToKarax(sourceFilePath, targetDir: string) =
+  echo &"Updating {targetDir/sourceFilePath.splitPath.tail}"
   let 
-    mdFileName = &"{dir}/{file}.md"
-    htmlFileName = &"{dir}/{file}.html"
-    nimFileName = &"src/website/{dir}/{file}.nim"
+    sourceFileName = sourceFilePath.splitFile.name
+    mdFileName = sourceFilePath
+    htmlFileName = sourceFilePath.changeFileExt "html"
+    nimFileName = targetDir/sourceFileName.changeFileExt("nim")
     currentDate = now().format("dd-MM-yyyy HH:mm")
   discard execCmd(&"./nimbledeps/bin/markdown < {mdFileName} > {htmlFileName}")
   discard execCmd(&"./nimbledeps/bin/html2karax {htmlFileName} --out:{nimFileName}")
@@ -32,41 +26,37 @@ proc createDom*(): VNode =
   writeFile(nimFileName, nimFile)
   removeFile(htmlFileName)
 
-# this watcher is already pretty good
-# it only updates a .nim file, if the hash of that file has changed
-# it uses the hashes.json file to sustain state through sessions. 
-# hashes.json is currently not commited
-# also it only updates the hashes.json if necessary
-# all this works nicely with the new "last updated" timestamp in the content files
-# which is in turn only updated, once a content files's contents change
-# TODO: what's still missing is a system to delete the entry in contentCollection.nim
-# and the src/website/content/*.nim file once a content/*.md file has been deleted
-proc watch(interval: int) =
-  var hashesJsonHash = contentFiles.toJson.secureHash
-  while true:
-    sleep(interval)
-    var ccRead = readFile("src/website/contentCollection.nim")
-    for p in walkDir("./content"):
-      let 
-        (dir, file, _) = p.path.splitFile
-        ccTemplate = &"""
-import content/{file}
-contents.add Content(name: "{file}", content: {file}.createDom)
+  const ccFileName = "src/website/contentCollection.nim"
+  var
+    ccRead = ccFileName.readFile
+    ccTemplate = &"""
+import content/{sourceFileName}
+contents.add Content(name: "{sourceFileName}", content: {sourceFileName}.createDom)
 """
-      if file notin contentFiles or 
-        contentFiles[file] != p.path.secureHashFile.`$`:
-        contentFiles[file] = p.path.secureHashFile.`$`
-        md2karax(dir, file)
-        if ccRead.find(ccTemplate) < 0:
-          let ccAppend = open("src/website/contentCollection.nim", fmAppend)
-          write(ccAppend, ccTemplate)
-          ccAppend.close()
+  if ccRead.find(ccTemplate) < 0:
+    let ccAppend = ccFileName.open(fmAppend)
+    ccAppend.write(ccTemplate)
+    ccAppend.close()
 
-    let json = contentFiles.toJson
-    if hashesJsonHash != json.secureHash:
-      echo "something changed, updating json"
-      hashesJsonHash = json.secureHash
-      writeFile(jsonFile, json)
+proc rmKarax(sourceFilePath, targetDir: string) =
+  echo &"Removing {targetDir/sourceFilePath.splitPath.tail}"
+  let 
+    sourceFileName = sourceFilePath.splitFile.name
+    nimFileName = targetDir/sourceFileName.changeFileExt("nim")
+  nimFileName.removeFile
+
+  const ccFileName = "src/website/contentCollection.nim"
+  var
+    ccRead = ccFileName.readFile
+    ccTemplate = &"""
+import content/{sourceFileName}
+contents.add Content(name: "{sourceFileName}", content: {sourceFileName}.createDom)
+"""
+  ccFileName.writeFile(ccRead.replace(ccTemplate, ""))
 
 when isMainModule:
-  watch(5000)
+  const
+    sourceDir = "content"
+    targetDir = "src/website/content"
+    jsonFile = "../hashes.json"
+  watch(sourceDir, targetDir, 5000, mdToKarax, rmKarax, jsonFile)
